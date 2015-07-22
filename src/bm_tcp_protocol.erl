e7c1eaffddf2ad311ff395bda3d3168f15b4ab50
Philip Cristiano <philipcristiano@gmail.com>
2015-07-22 17:32:49 -0400

    Send data to web interface

--- src/bm_tcp_protocol.erl
+++ src/bm_tcp_protocol.erl
@@ -18,40 +18,52 @@ init(Ref, Socket, Transport, _Opts = []) ->
 	loop(Socket, Transport, #state{buffer = <<"">>}).
 
 loop(Socket, Transport, State) ->
+    lager:debug("Loop state ~p~n", [State]),
 	case Transport:recv(Socket, 0, 3600000) of
 		{ok, Data} ->
-            {ok, NewState} = handle_data(State, Data),
+            {ok, NewState} = handle_data(Data, State),
 			loop(Socket, Transport, NewState);
 		A ->
             lager:debug("Graphite huh? ~p~n", [A]),
 			ok = Transport:close(Socket)
 	end.
 
-handle_data(State=#state{buffer=Buffer}, NewData) ->
+handle_data(NewData, State=#state{buffer=Buffer}) ->
     lager:debug("Handle data: ~p~n", [NewData]),
     Data = <<Buffer/binary, NewData/binary>>,
-    Unprocessed = parse_data(Data),
-    {ok, State#state{buffer=Unprocessed}}.
+    {ok, NewState} = parse_data(Data, State),
+    lager:debug("Done parsing"),
+    {ok, NewState}.
 
-parse_data(Data) ->
+parse_data(Data, State) ->
     lager:debug("Parse binary: ~p~n", [Data]),
     case binary:split(Data, <<"\n">>) of
-    [_] ->
-        Data;
-    [Line, Rest] ->
-        write_message(Line),
-        parse_data(Rest)
+        [Line, Rest] ->
+            {ok, NewState} = parse_line(Line, State),
+            parse_data(Rest, NewState);
+        [_] ->
+            lager:debug("No match"),
+            {ok, State#state{buffer=Data}}
     end.
 
-write_message(Message) ->
-    lager:debug("Parsing message: ~p~n", [Message]),
+parse_line(Line, State) ->
+    [Type| Rest] = binary:split(Line, [<<":">>], [global]),
+    {ok, NewState} = handle_message(Type, Rest, State),
+
     %% [Metric, Value, TS] = binary:split(Messages, [<<" ">>], [global]),
     %% NumTS = case erlang:binary_to_integer(TS) of
     %%             0 -> get_timestamp();
     %%             A -> A
     %% end,
     %% bm_tsdb_server:write(Metric, NumTS, etsdb_numbers:to_float(Value)),
-    ok.
+    {ok, NewState}.
+
+handle_message(<<"device">>, [DeviceID], State) ->
+    {ok, State#state{device=DeviceID}};
+handle_message(<<"temperature (c)">>, [Sensor, Value], State) ->
+    lager:debug("Temp State: ~p~n", [State]),
+    bm_publish_metrics:publish_temperature(Sensor, Value),
+    {ok, State}.
 
 %% get_timestamp() ->
 %%   {Mega, Sec, _Micro} = os:timestamp(),
