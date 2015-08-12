3df42b70546b33c01bcf5ad5c871c1ebb6bcee0b
Philip Cristiano <philipcristiano@spotify.com>
2015-08-12 10:43:42 -0400

    Remove broadcast based protocol

--- src/bm_publish_metrics.erl
+++ src/bm_publish_metrics.erl
@@ -2,17 +2,9 @@
 
 -compile([{parse_transform, lager_transform}]).
 
--export([publish_temperature/2,
-         publish_temperature/4,
+-export([publish_temperature/4,
          settable_parameter/5]).
 
-
-publish_temperature(ID, Temp) ->
-    T = etsdb_numbers:to_float(Temp),
-    Topic = {bm_devices, ID},
-    pg2:create(Topic),
-    Pids = pg2:get_members(Topic),
-    send(pipe, Pids, temperature, {ID, T}).
 
 publish_temperature(Device, Sensor, TS, Temp) ->
     _ = lager:debug("Publishing temperature ~p~n", [{Device, Sensor, TS, Temp}]),
