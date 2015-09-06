38a6fc46380c0404bfda3635f8682401a5c95ac6
Philip Cristiano <philipcristiano@gmail.com>
2015-09-05 20:24:37 -0400

    Finish removing bc listener

--- src/bm_sup.erl
+++ src/bm_sup.erl
@@ -10,11 +10,5 @@ start_link() ->
 init([]) ->
 	Procs = [{bm_sup_connections,
               {bm_sup_connections, start_link, []},
-               permanent, 5000, supervisor, [bm_sup_connections]},
-             %{bm_mqtt_spike,
-             % {bm_mqtt_spike, start_link, []},
-             %  permanent, 5000, worker, [bm_mqtt_spike]},
-             {bm_protocol_bc_listener,
-              {bm_protocol_bc_listener, start_link, []},
-               permanent, 5000, worker, [bm_protocol_bc_listener]}],
+               permanent, 5000, supervisor, [bm_sup_connections]}],
 	{ok, {{one_for_one, 1, 5}, Procs}}.
